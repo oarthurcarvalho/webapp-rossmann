cec55185b0be48f70be992b1aa6a88ddb888d31d
Arthur Carvalho <arthur.reis@live.com>
2024-08-15 16:22:12 -0300

    edit handler.py

--- handler.py
+++ handler.py
@@ -41,5 +41,5 @@ def rossmann_predict():
         return Response( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    os.environ.get( 'PORT', 5000 )
-    app.run( host='0.0.0.0', port=PORT )
+    port = os.environ.get( 'PORT', 5000 )
+    app.run( host='0.0.0.0', port=port )
